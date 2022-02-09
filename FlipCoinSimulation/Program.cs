92f6c30f7633ab9d2fae5fe1480655677d79c337
Amitmagadum1545 <amitmagadum1008@gmail.com>
2022-02-09 12:00:08 +0530

    Added Welcome Message

---
+++ FlipCoinSimulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoinSimulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Flip Coin Simulation Program");
+            Console.ReadLine();
+        }
+    }
+}
